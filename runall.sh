ff0f0741433a3911c76c0984c2ad43f2b374dd3b
Troy Will <troydwill@gmail.com>
2010-10-16 05:21:23 -0700

    minor

--- runall.sh
+++ runall.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+source rrp-lib.sh
 for SCRIPT in 010_controller_welcome.sh \
     020_scaffold_post.sh \
     030_scaffold_model.sh \
     040_scaffold_make.sh \
     050_scaffold_city.sh; do
+    ${EDITOR} ${SCRIPT}
     echo "=> Running /bin/bash ${SCRIPT}"
     /bin/bash ${SCRIPT}
 done
