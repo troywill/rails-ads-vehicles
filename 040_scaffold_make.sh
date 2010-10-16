8f65d8016f019fb39f060e9d11308c0ce958c4e2
Troy Will <troydwill@gmail.com>
2010-10-16 14:44:30 -0700

    minor

--- 040_scaffold_make.sh
+++ 040_scaffold_make.sh
@@ -1,24 +1,23 @@
+#!/bin/bash
 source ./rrp-lib.sh
-NAME='make'
+MODEL='make'
 
 function do_generate () {
-    rails generate scaffold ${NAME} name:string url:string
-    rake db:migrate
+    rails generate scaffold ${MODEL} name:string url:string
 }
 
 function edit_model () {
-    MODEL="${TOP_DIR}/app/models/${NAME}.rb"
-    cat >> ${MODEL} <<EOF
+    MODEL_RB="${TOP_DIR}/app/models/${MODEL}.rb"
+    cat >> ${MODEL_RB} <<EOF
   validates_presence_of :name
   validates_uniqueness_of :name
   has_many :models
 EOF
 
-    $EDITOR ${MODEL}
+    $EDITOR ${MODEL_RB}
 }
 
 function do_migration () {
-    echo "Run db:migrate?"
     rake db:migrate
 }
 
