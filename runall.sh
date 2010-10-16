a65ad0d23edaf167db6139f59f8edaf068ff29df
Troy Will <troydwill@gmail.com>
2010-10-16 04:51:04 -0700

    minor

--- runall.sh
+++ runall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 for SCRIPT in 010_controller_welcome.sh \
     020_scaffold_post.sh \
-    030_scaffold_make.sh \
-    040_scaffold_model.sh \
+    030_scaffold_model.sh \
+    040_scaffold_make.sh \
     050_scaffold_city.sh; do
     echo "=> Running /bin/bash ${SCRIPT}"
     /bin/bash ${SCRIPT}
