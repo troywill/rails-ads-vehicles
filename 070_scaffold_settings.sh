b832ec8782b8052bc2ba9d96e5b6017b815de908
Troy Will <troydwill@gmail.com>
2010-10-17 14:59:01 -0700

    minor

--- 070_scaffold_settings.sh
+++ 070_scaffold_settings.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 source ./rrp-lib.sh
-MODEL='city'
+MODEL="**edit_me**"
 
 function generate_scaffold () {
-    rails generate scaffold ${MODEL} \
-	name:string \
-	lat:float \
-	long:float \
-	travel_time:integer \
-	google_link:string \
-	google_embed:string
+    rails generate scaffold ${MODEL} name:string \
+	username:string \
+	password:string \
+	email:string
 }
 
 function edit_model () {
     MODEL_RB="${TOP_DIR}/app/models/${MODEL}.rb"
     cat >> ${MODEL_RB} <<EOF
-  validates_presence_of :name
-  has_many :posts
+
+# validates_presence_of :name
+# validates_presence_of :username
+# validates_uniqueness_of :username
+# validates_presence_of :password
 EOF
 
     $EDITOR ${MODEL_RB}
+
     echo "Don't forget to edit foreign key model, if applicable"
     
 }
 
 function do_migration () {
-    echo "Run db:migrate?"
     rake db:migrate
 }
 
