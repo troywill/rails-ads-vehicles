fb23c2e22b8ae025b21628db23d8ea71c41fa774
Troy Will <troydwill@gmail.com>
2010-10-21 13:27:39 -0700

    load_default_user_data.rb: edit this and load or require with rails console providing absolute path

---
+++ load_data/load_default_user_data.rb
@@ -0,0 +1,22 @@
+# rails console, either
+# load '/full/path/to/this/file.rb'
+#             or
+# require '/full/path/to/this/file.rb'
+
+username = 'troy'
+password = 'troy'
+
+User.create( :name => 'Troy Will', 
+             :username => username',
+             :password => password',
+             :email => 'troydwill@gmail.com',
+             :homepage => 'http://troywill.com',
+             :twitter => 'http://twitter.com/troydwill',
+             :facebook => 'http://facebook.com/troydwill',
+             :share_models => 'true',
+             :share_contact => 'true',
+             :zip_code => 91711
+)
+
+user = User.find_by_username(username)
+
---
+++ load_data/troy.rb
@@ -0,0 +1,7 @@
+# rails console, either
+# load '/full/path/to/this/file.rb'
+#             or
+# require '/full/path/to/this/file.rb'
+
+user = User.find_by_username('troy')
+
