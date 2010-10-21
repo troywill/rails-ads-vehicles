550db3b218ed4de9e156ba764eceb11adeb5fbe0
Troy Will <troydwill@gmail.com>
2010-10-21 13:54:35 -0700

    runall.sh: new scripts for user

--- runall.sh
+++ runall.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-# <troydwill@gmail.com> October 2010
+# <troydwill@gmail.com> October 21, 2010
 
 source ./rrp-lib.sh
 
 for SCRIPT in 010_controller_welcome.sh \
-    020_scaffold_ad.sh \
-    030_scaffold_model.sh \
-    040_scaffold_make.sh \
-    050_scaffold_city.sh \
-    060_scaffold_search.sh \
-    070_scaffold_settings.sh \
-    080_scaffold_links.sh; do
+    020_scaffold_user.sh \
+    030_identify_user.sh \
+    040_login_form.sh \
+    050_edit_login_in_welcome.sh \
+    060_add_authenticate_method_to_user.sh \
+    070_scaffold_ad.sh \
+    080_scaffold_color.sh \
+    090_scaffold_model.sh \
+    100_scaffold_make.sh \
+    110_scaffold_city.sh \
+    120_scaffold_search.sh \
+    130_scaffold_settings.sh \
+    140_scaffold_links.sh; do
     ${EDITOR} ${SCRIPT}
-    echo "=> Run /bin/bash ${SCRIPT}?: <any key to continue>";
-    read $answer
+    read -p "Run /bin/bash ${SCRIPT}?: <any key to continue> ";
     /bin/bash ${SCRIPT}
 done
