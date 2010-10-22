2822b14d8ce402685797c11071ddea0827f1b310
Troy Will <troydwill@gmail.com>
2010-10-21 18:34:26 -0700

    new file:   160_scaffold_system_colors.sh

---
+++ 160_scaffold_system_colors.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+source ./rrp-lib.sh
+MODEL="system_color"
+
+function generate_scaffold () {
+    rails generate scaffold ${MODEL} \
+	name:string \
+	wikipedia:string
+}
+
+function edit_model () {
+    MODEL_RB="${TOP_DIR}/app/models/${MODEL}.rb"
+    cat >> ${MODEL_RB} <<EOF
+validates :name, :presence => true, :uniqueness => true
+EOF
+
+    $EDITOR ${MODEL_RB}
+    echo "Don't forget to edit foreign key model, if applicable"
+}
+
+###### Main program #######
+
+generate_scaffold
+edit_model
+read -p "Run rake db:migrate? " && rake db:migrate
+
