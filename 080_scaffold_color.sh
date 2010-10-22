73ec1b8a12e6a8237e979f5bed34932a6238630e
Troy Will <troydwill@gmail.com>
2010-10-21 19:19:23 -0700

    deleted:    160_scaffold_system_colors.sh

--- 080_scaffold_color.sh
+++ 080_scaffold_color.sh
@@ -4,14 +4,12 @@ MODEL="color"
 
 function generate_scaffold () {
     rails generate scaffold ${MODEL} \
-	user_id:integer \
 	name:string
 }
 
 function edit_model () {
     MODEL_RB="${TOP_DIR}/app/models/${MODEL}.rb"
     cat >> ${MODEL_RB} <<EOF
-validates :user_id, :presence => true, :numericality => true
 validates :name, :presence => true, :uniqueness => true
 EOF
 
