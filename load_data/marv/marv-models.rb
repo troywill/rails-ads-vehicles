b6893a9da4db0e3c7ad04ac077ab3ae4550959de
Troy Will <troydwill@gmail.com>
2010-10-23 13:42:33 -0700

    new file:   marv-models.rb renamed:    searches-marv.rb -> marv-searches.rb

---
+++ load_data/marv/marv-models.rb
@@ -0,0 +1,12 @@
+username = 'marv'
+user = User.find_by_username(username)
+
+# Available makes: 
+# Chrysler
+# Chevrolet
+
+# Create some vehicle models Suzuki Burgman
+Model.create( :user_id => user_id, :make_id => Make.find_by_name('Chrysler').id, :name => 'Sebring Convertible' )
+Model.create( :user_id => user_id, :make_id => Make.find_by_name('Chevrolet').id, :name => 'Corvette' )
+Model.create( :user_id => user_id, :make_id => Make.find_by_name('Chevrolet').id, :name => 'SSR' )
+
