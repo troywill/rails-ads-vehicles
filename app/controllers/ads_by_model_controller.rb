c75120b331e2e1ba9664398073351fcef0642e98
Troy Will <troydwill@gmail.com>
2010-10-23 02:12:30 -0700

    modified:   app/controllers/ads_by_model_controller.rb modified:   app/views/layouts/application.html.erb modified:   load_data/cities.rb

--- app/controllers/ads_by_model_controller.rb
+++ app/controllers/ads_by_model_controller.rb
@@ -1,6 +1,7 @@
 class AdsByModelController < ApplicationController
   def index
-    @ads = Ad.find_all_by_user_id(session[:user_id])
+    @ads = Ad.find_all_by_user_id(session[:user_id], :order => "id DESC")
     @models = Model.find_all_by_user_id(session[:user_id])
   end
 end
+
--- load_data/cities.rb
+++ load_data/cities.rb
@@ -1,4 +1,6 @@
 City.create( :name =>'Bellflower' ,:lat=>33.888056,:long=> -118.1275 )
 City.create( :name =>'Garden Grove',:lat=>33.778889,:long=> -117.960278 )
+City.create( :name => 'Northridge', :lat=>34.228056, :long=>-118.535833 )
 City.create( :name => 'Pasedena', :lat=> 34.156111, :long=> -118.131944 )
+City.create( :name => 'Pacoima', :lat=> 34.2625, :long => -118.42611 )
 City.create( :name =>'Yorba Linda',:lat=> 33.888551,:long=> -117.813231 )
