a24e6aa9395d596a58d7a91867c22322b75ee23b
Troy Will <troydwill@gmail.com>
2010-10-23 14:13:00 -0700

    modified:   ../marv/marv-models.rb modified:   searches-troy.rb

--- load_data/marv/marv-models.rb
+++ load_data/marv/marv-models.rb
@@ -7,5 +7,5 @@ user = User.find_by_username(username)
 
 # Create some vehicle models Suzuki Burgman
 Model.create( :user_id => user.id, :make_id => Make.find_by_name('Chrysler').id, :name => 'Sebring Convertible' )
-Model.create( :user_id => user.id, :make_id => Make.find_by_name('Chevrolet').id, :name => 'Corvette' )
+Model.create( :user_id => user.id, :make_id => Make.find_by_name('Chevrolet').id, :name => 'Corvette Convertible' )
 Model.create( :user_id => user.id, :make_id => Make.find_by_name('Chevrolet').id, :name => 'SSR' )
--- load_data/troy/searches-troy.rb
+++ load_data/troy/searches-troy.rb
@@ -11,3 +11,8 @@ Search.create( :user_id => user.id,
                :name => "Burgman, Craig's List Inland Empire",
                :url => 'http://inlandempire.craigslist.org/search/?areaID=104&subAreaID=&query=Burgman&catAbb=sss',
                )
+
+Search.create( :user_id => user.id,
+               :name => "Burgman, Craig's List Los Angeles",
+               :url => 'http://losangeles.craigslist.org/search/mcy?query=Burgman&srchType=A&minAsk=&maxAsk='
+               )
