15d305b6d5469bc1dc53da36415eb9b6321e0a18
Troy Will <troydwill@gmail.com>
2010-10-23 18:52:41 -0700

    cities.rb: Los Angeles

--- app/controllers/ads_by_model_controller.rb
+++ app/controllers/ads_by_model_controller.rb
@@ -1,7 +1,6 @@
 class AdsByModelController < ApplicationController
   def index
-    @ads = Ad.find_all_by_user_id(session[:user_id], :order => "id DESC")
+    @ads = Ad.find_all_by_user_id(session[:user_id], :order => "year DESC, ad_price DESC")
     @models = Model.find_all_by_user_id(session[:user_id])
   end
 end
-
