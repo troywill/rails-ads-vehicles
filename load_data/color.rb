a4fef7d3651a581a6b9acfe7121e99ad6239b017
Troy Will <troydwill@gmail.com>
2010-10-25 11:48:39 -0700

    color.rb: Yellow

--- load_data/color.rb
+++ load_data/color.rb
@@ -1,3 +1,4 @@
 Color.create(
-             [{ :name => 'Silver' }, { :name => 'White' }, { :name => 'Gray' }, { :name => 'Green' }, { :name => 'Blue' }, { :name => 'Black' }, {:name => 'Red' }]
+             [{ :name => 'Silver' }, { :name => 'White' }, { :name => 'Gray' }, { :name => 'Green' }, { :name => 'Blue' }, { :name => 'Black' }, {:name => 'Red' }, {:name => 'Yellow' }]
+             
              )
