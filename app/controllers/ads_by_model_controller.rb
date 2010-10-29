71bf348b6030edea5f3609e280c8467b66aae7fb
Troy Will <troydwill@gmail.com>
2010-10-29 12:23:15 -0700

    cities.rb: Burbank

--- app/controllers/ads_by_model_controller.rb
+++ app/controllers/ads_by_model_controller.rb
@@ -1,6 +1,8 @@
 class AdsByModelController < ApplicationController
   def index
-    @ads = Ad.find_all_by_user_id(session[:user_id], :order => "year DESC, ad_price DESC")
+    days = 3
+    time = (Time.now - (days * 86400)).iso8601
+    @ads = Ad.find_all_by_user_id(session[:user_id], :conditions => "created_at > '#{time}'", :order => "year DESC, ad_price DESC")
     @models = Model.find_all_by_user_id(session[:user_id])
   end
 end
