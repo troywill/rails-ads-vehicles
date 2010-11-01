3366ddc725c9e7f4c36493922a2e1f999da55b89
Troy Will <troydwill@gmail.com>
2010-10-31 23:52:02 -0700

    new file:   ../180_controller_frame_ad.sh modified:   ../app/controllers/ads_by_model_controller.rb modified:   ../app/views/layouts/application.html.erb modified:   cities.rb: Palm Desert

--- app/controllers/ads_by_model_controller.rb
+++ app/controllers/ads_by_model_controller.rb
@@ -1,9 +1,9 @@
 class AdsByModelController < ApplicationController
   def index
-    days = 30
+    days = 3
     time = (Time.now - (days * 86400)).iso8601
-#    @ads = Ad.find_all_by_user_id(session[:user_id], :conditions => "created_at > '#{time}'", :order => "year DESC, ad_price DESC")
-    @ads = Ad.find_all_by_user_id(session[:user_id], :order => "year DESC, ad_price DESC")
+    @ads = Ad.find_all_by_user_id(session[:user_id], :conditions => "created_at > '#{time}'", :order => "year DESC, ad_price DESC")
+    # @ads = Ad.find_all_by_user_id(session[:user_id], :order => "year DESC, ad_price DESC")
     @models = Model.find_all_by_user_id(session[:user_id])
   end
 end
