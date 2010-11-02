fdd10d7c684aee9b44de2c2bcecf9aaa5a59754c
Troy Will <troydwill@gmail.com>
2010-11-02 14:43:37 -0700

    cities.rb Culver City

--- app/controllers/ads_by_model_controller.rb
+++ app/controllers/ads_by_model_controller.rb
@@ -1,9 +1,11 @@
 class AdsByModelController < ApplicationController
   def index
-    days = 3
+    user_id = session[:user_id]
+    days = 30
     time = (Time.now - (days * 86400)).iso8601
-    @ads = Ad.find_all_by_user_id(session[:user_id], :conditions => "created_at > '#{time}'", :order => "year DESC, ad_price DESC")
-    @ads = Ad.find_all_by_user_id(session[:user_id], :conditions => ['evaluated = ? OR interesting = ?', false, true], :order => "year DESC, ad_price DESC")
-    @models = Model.find_all_by_user_id(session[:user_id])
+    @ads = Ad.find_all_by_user_id(user_id, :conditions => "created_at > '#{time}'", :order => "year DESC, ad_price DESC")
+    @ads = Ad.find_all_by_user_id(user_id, :conditions => ["(evaluated = ? OR interesting = ?) AND created_at > '#{time}'", false, true], :order => "year DESC, ad_price DESC")
+
+    @models = Model.find_all_by_user_id(user_id)
   end
 end
